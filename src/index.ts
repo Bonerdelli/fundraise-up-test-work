94a6279b12031b0ca17732be1a115c4b27679054
Andrey Nekrasov <bonerdelli@gmail.com>
2024-03-30 13:54:38 +0100

    Add draft rendering functions

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,35 @@ function testComponent(): Element {
   return element
 }
 
+function createButton(letter: string): Element {
+  // const host = document.body
+  // const shadow = host.attachShadow({ mode: 'open' })
+  const div = document.createElement('div')
+  div.classList.add('btn', 'btn-primary')
+  div.textContent = letter
+  // shadow.appendChild(div)
+  return div
+}
+
+function testInit() {
+  const currentQuestionEl = document.getElementById('current_question')
+  const totalQuestionsEl = document.getElementById('total_questions')
+  const answerEl = document.getElementById('answer')
+  const lettersEl = document.getElementById('letters')
+
+  if (!currentQuestionEl || !totalQuestionsEl || !answerEl || !lettersEl) {
+    throw new Error('Missed required elements')
+  }
+
+  currentQuestionEl.innerText = '10'
+  totalQuestionsEl.innerText = '100'
+  answerEl.innerText = 'jfosie'
+  // lettersEl.lettersEl.appendChild()
+  // lettersEl.innerText = 'a'
+
+  const testButton = createButton('a')
+  lettersEl.appendChild(testButton)
+}
+
 document.body.appendChild(testComponent())
+testInit()
