e9e35ae404d309749e26e265c1d018171c2c0f93
Andrey Nekrasov <bonerdelli@gmail.com>
2024-04-01 09:44:01 +0200

    Add generation of game set and basic state handling

--- src/VocabularyTrainer/Application.ts
+++ src/VocabularyTrainer/Application.ts
@@ -1,47 +1,66 @@
 import {
   Application,
   ApplicationOptions,
+  Character,
   CharacterState,
+  GameResult,
+  GameRoundState,
   Renderer,
 } from './types'
 
 export class VocabularyTrainer implements Application {
   protected words: string[]
   protected currentWords?: string[]
-  protected currentGameSet?: Record<string, string[]>
+  private gameState: GameRoundState[] = []
   private renderer: Renderer
 
-  wordsInGame: number
-  currentWordNum: number
-  currentWordErrors?: number
-  erroredResultsCount?: number
+  public wordsInGame: number
+  public currentRoundNum: number
+  public currentWordLetters: string[] = []
+  public currentWordErrors = 0
+  public erroredResultsCount = 0
 
   constructor(opts: ApplicationOptions) {
     this.words = opts.words
     this.renderer = opts.renderer
     this.wordsInGame = opts.wordsInGame
-    this.currentWordNum = 0
+    this.currentRoundNum = 0
   }
 
-  runNewGame() {
+  public runNewGame() {
     this.generateGameSet(this.wordsInGame)
     this.currentWordErrors = 0
     this.erroredResultsCount = 0
-    this.currentWordNum = 1
-    this.nextWord()
+    this.currentRoundNum = 0
+    this.renderWord()
+  }
+
+  protected get currentRound(): GameRoundState | null {
+    return this.gameState[this.currentRoundNum - 1] ?? null
+  }
+
+  protected get currentWord(): string | null {
+    const word = this.gameState[this.currentRoundNum]?.shuffledWord
+    if (word) {
+      return word.join('')
+    }
+    return null
   }
 
   protected generateGameSet(wordsLimit?: number) {
-    const currentWords = this.getRandomizedArray(this.words, wordsLimit)
-    const wordsWithShuffledChars = currentWords.map(word =>
-      this.getRandomizedArray(word.split('')).join(''),
-    )
-    this.currentWords = wordsWithShuffledChars
-    // this.currentGameSet =
+    const currentWords = this.shuffleArray(this.words, wordsLimit)
+    const initialGameSet: GameRoundState[] = currentWords.map(word => ({
+      state: GameResult.InProgress,
+      originalWord: word.split(''),
+      shuffledWord: this.shuffleArray(word.split('')),
+      suggestedCharacters: [],
+      errorsCount: 0,
+    }))
+    this.gameState = initialGameSet
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected getRandomizedArray(input: any[], limit?: number) {
+  protected shuffleArray(input: any[], limit?: number) {
     let result = [...input]
     result.sort(() => Math.random() - 0.5)
     if (limit && limit < input.length) {
@@ -50,25 +69,49 @@ export class VocabularyTrainer implements Application {
     return result
   }
 
-  protected nextWord() {
-    const word = this.currentWords?.pop()
+  private renderWord() {
+    const word = this.currentWord
     if (!word) {
       // TODO: set completion
       return
     }
     const characterInstances = this.renderer.renderWord(word)
     for (const instance of characterInstances) {
-      instance.setOnSelect(() => instance.setState(CharacterState.Error))
+      instance.setOnSelect((...args) => this.handleCharacterSelection(...args))
     }
-    this.currentWordNum++
+    this.currentRoundNum++
   }
 
-  // protected checkIsCharacterValid(char: string) {
+  private nextWord() {
+    this.currentRoundNum++
+    this.renderWord()
+  }
 
-  // }
+  private handleCharacterSelection(
+    char: string,
+    index: number,
+    instance: Character,
+  ) {
+    const round = this.currentRound as GameRoundState
+    if (this.checkIsCharacterValid(char)) {
+      instance.setState(CharacterState.Success)
+      round.suggestedCharacters.push(char)
+    } else {
+      instance.setState(CharacterState.Error)
+      round.errorsCount += 1
+    }
+  }
 
-  protected getRandomWord() {
-    const index = Math.floor(Math.random() * this.words.length)
-    return this.words[index]
+  private checkIsCharacterValid(char: string): boolean {
+    const currentRound = this.currentRound
+    if (!currentRound) {
+      return false
+    }
+    const position = currentRound.suggestedCharacters.length
+    console.log('position', position, currentRound.originalWord, this.gameState, char)
+    if (currentRound.originalWord[position] === char) {
+      return true
+    }
+    return false
   }
 }
